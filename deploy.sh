737b54ffde6020783b97c35c9f7a8b55ba59925a
Asadbek Nazarov <nazarov_asadbek_job@mail.ru>
2022-02-22 01:25:08 +0500

    1) Deployment Tag: v0.0.1 Spent time: 2 hours

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ cd dist
 
 git init
 git add -A
-git commit -m 'New Deployment'
+git commit -m 'New Deployment 2'
 git push -f https://github.com/NazarovAsadbek/project-repository.git master:gh-pages
 
 cd -
